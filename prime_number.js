cbb00b4f632fac9fb9f7f4851cb9a4f82e7e65de
Reynold Figueroa <reynold0801@outlook.com>
2022-05-28 09:28:57 -0400

    .

---
+++ prime_number.js
@@ -0,0 +1,30 @@
+//? Determine whether a given number is prime
+
+function isPrime(number) {
+  if (isNaN(parseInt(number))) return false;
+  if (number % 1 > 0) return false;
+  if (number < 2) return false;
+
+  // for (let i = 2; i < number; i++) {
+  //   if (number % i === 0) return false;
+  // }
+
+  //* Improvement: Since there is a rule that all non prime numbers follow [n === sqrt(n) * sqrt(n)]
+  //* We can improve the worst case scenario by iterating until we get to the sqrt of the given number instead of all smaller numbers.
+  for (let i = 2; i < Math.sqrt(number); i++) {
+    if (number % i === 0) return false;
+  }
+
+  return true;
+}
+//* Best case: Constant complexity O(1)
+//* Average case: Linear complexity O(n) | Improved = O(sqrt(n))
+//* Worst case: Linear complexity O(n) | Improved = O(sqrt(n))
+console.log(
+  isPrime(2),
+  isPrime("abc"),
+  isPrime(5.2),
+  isPrime(-12),
+  isPrime(200),
+  isPrime(27277)
+);
--- sum_of_array.js
+++ sum_of_array.js
@@ -1,11 +1,10 @@
 //? You are given an array of numbers and you have to retrun sum of all the numbers
 
 function arraySum(arr, sum = 0) {
-    if (arr.length === 0) return sum;
-    sum += arr[0];
-    arr.shift();
-    return arraySum(arr, sum);
+  if (arr.length === 0) return sum;
+  sum += arr[0];
+  arr.shift();
+  return arraySum(arr, sum);
 }
-//* Linear complexity O(n) 
+//* Linear complexity  O(n)
 console.log(arraySum([2, 3, 5, 10, 20]));
-
